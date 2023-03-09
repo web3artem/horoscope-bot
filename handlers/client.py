76bcff5aab5abd008e45eeafc8c2b5783f79149b
artemcringe <tyoma.dar@gmail.com>
2023-03-10 00:36:19 +0300

    Realised some functions in client.py, need to work with birthdate_func

--- handlers/client.py
+++ handlers/client.py
@@ -2,45 +2,75 @@ from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.types import InputFile, CallbackQuery
+from aiogram.types import CallbackQuery
 
 from create_bot import bot
 from keyboards.client_kb import client_gender_inline_kb
+from services.funcs import get_background_photo, validate_birthdate
 
 
 class FSMClient(StatesGroup):
     client_name = State()
     client_gender = State()
     client_birth_date = State()
+    client_birth_place = State()
 
 
 async def start_command(message: types.Message):
-    photo_path = 'media/backgrounds/background-name.jpg'
-    photo = InputFile(photo_path)
     await FSMClient.client_name.set()
-    await bot.send_photo(chat_id=message.from_user.id,
-                         photo=photo)
+    await get_background_photo(message, 'media/backgrounds/background-name.jpg')
 
 
 async def name_info(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['name'] = message.text
 
-    photo_path = 'media/backgrounds/background-gender.jpg'
-    photo = InputFile(photo_path)
     await FSMClient.next()
-    await bot.send_photo(chat_id=message.from_user.id,
-                         photo=photo,
-                         reply_markup=client_gender_inline_kb)
+    await get_background_photo(message, 'media/backgrounds/background-gender.jpg', reply_markup=client_gender_inline_kb)
 
 
 async def gender_info(callback_query: CallbackQuery, state: FSMContext):
     gender = callback_query.data
+    async with state.proxy() as data:
+        data['gender'] = gender
     await FSMClient.next()
-    await callback_query.answer("Your gender has been recorded!")
+
+    await get_background_photo(callback_query, 'media/backgrounds/birthdate-img.jpg',
+                               caption='Пожалуйста, введите дату своего рождения в формате 15.11.2001')
+
+
+"""Необходимо реализовать хендлер, который будет обрабатывать некорректный и корректный ввод даты рождения."""
+
+# async def birth_date(message: types.Message, state: FSMContext):
+#     birthdate = message.text
+#     birth_format = '%d.%m.%Y'
+#     await FSMClient.next()
+#
+#     while True:  # Если дата введена корректно, то выходим из цикла
+#         try:
+#             datetime.strptime(birthdate, birth_format)  # проверка даты
+#             async with state.proxy() as data:
+#                 data['birth_date'] = birthdate
+#             break
+#         except ValueError:
+#             await get_background_photo(message, 'media/backgrounds/birthdate-img.jpg',
+#                                        caption='Пожалуйста, введите дату своего рождения в формате 15.11.2001')
+#
+#     await get_background_photo(message, 'media/backgrounds/birthplace-img.jpg',
+#                                caption='Пожалуйста, введите место своего рождения')
+
+
+# async def process_birthdate(message: types.Message, state: FSMContext):
+#     if validate_birthdate(message.text):
+#         await FSMClient.next()
+#     else:
+#         await FSMClient.previous()
+#         print(FSMClient.)
+    # await bot.send_message(text='Спасибо, все в порядке', chat_id=message.chat.id)
 
 
 def register_handlers_client(disp: Dispatcher):
     disp.register_message_handler(start_command, commands=['start', 'help'], state=None)
     disp.register_message_handler(name_info, state=FSMClient.client_name)
     disp.register_callback_query_handler(gender_info, Text(startswith='gender'), state=FSMClient.client_gender)
+    disp.register_message_handler(process_birthdate, state=FSMClient.client_birth_date)
